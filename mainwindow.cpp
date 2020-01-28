ff4963eeee92425df1ac1fd482a704220940c015
Jordan Mielbrecht <mielbrechtj@spu.edu>
2020-01-28 15:51:47 -0800

    Added calculateCost(), checkValidResInfo(), checkShowNext()

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->stackedWidget->setCurrentIndex(0); //First Page
+    ui->next->setVisible(false); //Next button is not displayed
 }
 
 MainWindow::~MainWindow()
@@ -14,38 +15,168 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-void MainWindow::on_plainTextEdit_textChanged()
+bool MainWindow::checkValidResInfo()
 {
-
+    return ((numAdults + numKids) > 0 && (numAdults + numKids) <= maxGuests && numNights > 0 && endDate > beginDate);
+}
+void MainWindow::checkShowNext()
+{
+    //If all reservation data is valid, make the next button visible.
+        ui->next->setVisible(checkValidResInfo());
+}
+ void MainWindow::calculateCost()
+{
+     double roomCost;
+     switch(roomType){
+       case 1:
+         if(parkingNeeded){
+           roomCost = (284 + 12.75)*numNights;
+         }
+         else{
+           roomCost = 284*numNights;
+         }
+         break;
+       case 2:
+       if(parkingNeeded){
+           roomCost = (325 + 12.75)*numNights;
+         }
+         else{
+           roomCost = 325*numNights;
+         }
+         break;
+       case 3:
+       if(parkingNeeded){
+           roomCost = (290 + 12.75)*numNights;
+         }
+         else{
+           roomCost = 290*numNights;
+         }
+         break;
+       case 4:
+       if(parkingNeeded){
+           roomCost = (350 + 12.75)*numNights;
+         }
+         else{
+           roomCost = 350*numNights;
+         }
+         break;
+       default:
+         roomCost = -1; //ERROR
+         break;
+     }
+     ui->est_cost_output->setText(QString::number(roomCost));
+}
+void MainWindow::on_next_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(1);
 }
 
-void MainWindow::on_queen_clicked()
+void MainWindow::on_diffBtn_clicked()
 {
-
+    ui->stackedWidget->setCurrentIndex(3);
 }
 
-void MainWindow::on_king_clicked()
+void MainWindow::on_needPkgChkBx_toggled(bool checked)
 {
-
+    parkingNeeded = checked;
+    calculateCost();
 }
 
-void MainWindow::on_standard_clicked()
+void MainWindow::on_exit_clicked() //EXIT BUTTON
 {
-
+    QApplication::quit();
 }
 
-void MainWindow::on_atrium_clicked()
+void MainWindow::on_nameInput_textChanged(const QString &arg1)
 {
+    QString message = arg1;
 
+        if (message.length() > 0){
+            customerName = message;
+        }
 }
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_queenStd_toggled(bool checked)
 {
-    ui->stackedWidget->setCurrentIndex(1);
+    if(checked){
+        roomType = 1;
+        maxGuests = 4;
+    }
+    calculateCost();
+    checkShowNext();
 }
 
-void MainWindow::on_pushButton_2_clicked()
+void MainWindow::on_queenAtr_toggled(bool checked)
+{
+    if(checked){
+        roomType = 2;
+        maxGuests = 4;
+    }
+    calculateCost();
+    checkShowNext();
+}
+
+void MainWindow::on_kngStd_toggled(bool checked)
+{
+    if(checked){
+        roomType = 3;
+        maxGuests = 3;
+    }
+    calculateCost();
+    checkShowNext();
+}
+
+void MainWindow::on_kngAtr_toggled(bool checked)
+{
+    if(checked){
+        roomType = 4;
+        maxGuests = 3;
+    }
+    calculateCost();
+    checkShowNext();
+}
+
+void MainWindow::on_numNights_valueChanged(int arg1)
+{
+    numNights = arg1;
+    calculateCost();
+    checkShowNext();
+}
+
+void MainWindow::on_beginDate_userDateChanged(const QDate &date)
+{
+    beginDate = date;
+    calculateCost();
+    checkShowNext();
+}
+
+void MainWindow::on_endDate_userDateChanged(const QDate &date)
+{
+    endDate = date;
+    calculateCost();
+    checkShowNext();
+}
+
+void MainWindow::on_bktoForm_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_pay_clicked()
 {
     ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_adultCt_valueChanged(int arg1)
+{
+    numAdults = arg1;
+    calculateCost();
+    checkShowNext();
+}
+
+void MainWindow::on_childCt_valueChanged(int arg1)
+{
+    numKids = arg1;
+    calculateCost();
+    checkShowNext();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDate>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,25 +17,54 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_plainTextEdit_textChanged();
 
-    void on_queen_clicked();
+    void on_nameInput_textChanged(const QString &arg1);
 
-    void on_king_clicked();
+    void on_next_clicked();
 
-    void on_standard_clicked();
+    void on_diffBtn_clicked();
 
-    void on_atrium_clicked();
+    void on_exit_clicked();
 
-    void on_pushButton_clicked();
 
-    void on_pushButton_2_clicked();
+    void on_queenStd_toggled(bool checked);
+
+    void on_queenAtr_toggled(bool checked);
+
+    void on_kngStd_toggled(bool checked);
+
+    void on_kngAtr_toggled(bool checked);
+
+    void on_numNights_valueChanged(int arg1);
+
+    void on_beginDate_userDateChanged(const QDate &date);
+
+    void on_endDate_userDateChanged(const QDate &date);
+
+    void on_needPkgChkBx_toggled(bool checked);
+
+    void on_bktoForm_clicked();
+
+    void on_pay_clicked();
+
+    void on_adultCt_valueChanged(int arg1);
+
+    void on_childCt_valueChanged(int arg1);
+
+    void calculateCost();
+    bool checkValidResInfo();
+    void checkShowNext();
 
 private:
     Ui::MainWindow *ui;
     QString customerName;
     int roomType;
     int numNights;
+    QDate beginDate;
+    QDate endDate;
+    int numAdults = 0; //Initialized to 0
+    int numKids = 0; //Initialized to 0
+    int maxGuests = 0; //Changes with room type; begins at 0 to prevent invalid "next" button visibility
     bool parkingNeeded;
 };
 #endif // MAINWINDOW_H
